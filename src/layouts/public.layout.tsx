7001424bbb08a7e3e58093ce547d57766cd8e0bc
DavyKing <yungdynamic53@gmail.com>
2023-06-15 23:36:31 +0100

    images and fixes

--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -1,7 +1,17 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+function replay(eventType: string) {
+  const event = new Event(eventType);
+  console.log(eventType);
+  window.dispatchEvent(event);
+}
+
 function PublicLayout({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    replay("DOMContentLoaded");
+    replay("load");
+  }, []);
   return (
     <div>
       <Header></Header>
